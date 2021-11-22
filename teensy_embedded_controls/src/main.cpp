196e95f07010bab22246668c36cce160dc588f83
David Michelman <daweim0@gmail.com>
2021-11-21 23:15:09 -0800

    code transfer

--- teensy_embedded_controls/src/main.cpp
+++ teensy_embedded_controls/src/main.cpp
@@ -10,25 +10,41 @@
 
 #include <std_msgs/msg/int32.h>
 
-// #include <Eigen.h>
+
+// nodelets will add themselves to these vectors (since all subscribers and publishers need to be initialized on startup)
+
+// micro_ros timer object, period (ms), timer callback (recieves timer_object, last_call_time as arguments)
+std::vector<std::tuple<rcl_timer_t, int, void *(rcl_timer_t*, int64_t)>> component_timers;
+
+// micro_ros subscription object, topic name, message type, callback function pointer, best effort/reliable
+//  (true is best effort, false is reliable)
+std::vector<std::tuple<rcl_subscription_t, std::string, rosidl_message_type_support_t*, void *(const void *msgin), bool>> component_subscribers;
+
+
+// includes to files containing subscriptions or timers must be below here
 
 #include <twist_to_thrusts.h>
 
-rcl_subscription_t subscriber;
-std_msgs__msg__Int32 msg;
 
-rcl_publisher_t publisher_internal;
+// rcl_subscription_t subscriber;
+// std_msgs__msg__Int32 msg;
 
-rcl_subscription_t subscriber_internal;
-std_msgs__msg__Int32 msg_internal;
+// rcl_publisher_t publisher_internal;
 
-rcl_publisher_t publisher;
+// rcl_subscription_t subscriber_internal;
+// std_msgs__msg__Int32 msg_internal;
+
+// rcl_publisher_t publisher;
+
+// rcl_timer_t timer;
 
 rclc_executor_t executor;
 rclc_support_t support;
 rcl_allocator_t allocator;
 rcl_node_t node;
-rcl_timer_t timer;
+
+
+
 
 #define LED_PIN 13
 
@@ -44,33 +60,32 @@ void error_loop(){
 }
 
 
-void subscription_callback(const void * msgin)
-{
-  const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
-  digitalWrite(LED_PIN, (msg->data == 0) ? LOW : HIGH);
+// void subscription_callback(const void * msgin)
+// {
+//   const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
+//   digitalWrite(LED_PIN, (msg->data == 0) ? LOW : HIGH);
   
-  RCCHECK(rcl_publish(&publisher_internal, msg, NULL));
-  // RCCHECK(rcl_publish(&publisher, msg, NULL));
-}
+//   RCCHECK(rcl_publish(&publisher_internal, msg, NULL));
+//   // RCCHECK(rcl_publish(&publisher, msg, NULL));
+// }
 
 
-void subscription_callback_internal(const void * msgin)
-{
-  const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
-  digitalWrite(LED_PIN, (msg->data == 0) ? LOW : HIGH);
+// void subscription_callback_internal(const void * msgin)
+// {
+//   const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
+//   digitalWrite(LED_PIN, (msg->data == 0) ? LOW : HIGH);
   
-  RCCHECK(rcl_publish(&publisher, msg, NULL));
-}
+//   RCCHECK(rcl_publish(&publisher, msg, NULL));
+// }
 
 
 void setup() {
-  int num_handles = 0;
-
   set_microros_transports();
   
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, HIGH);  
   
+  // put this delay here so the host micro_ros agent has time to conenct
   delay(2000);
 
   allocator = rcl_get_default_allocator();
@@ -81,38 +96,51 @@ void setup() {
   // create node
   RCCHECK(rclc_node_init_default(&node, "micro_ros_arduino_node", "", &support));
 
-  // create subscriber
-  RCCHECK(rclc_subscription_init_best_effort(
-//  RCCHECK(rclc_subscription_init_default(
-    &subscriber,
-    &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-    "/micro_ros_arduino_subscriber"));
-  num_handles++;  // SUPER IMPORTANT, DO NOT FORGET OR MICRO ROS WILL BREAK IN WEIRD WAYS
+//   // create subscriber
+//   RCCHECK(rclc_subscription_init_best_effort(
+// //  RCCHECK(rclc_subscription_init_default(
+//     &subscriber,
+//     &node,
+//     ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+//     "/micro_ros_arduino_subscriber"));
+//   num_handles++;  // SUPER IMPORTANT, DO NOT FORGET OR MICRO ROS WILL BREAK IN WEIRD WAYS
 
-  RCCHECK(rclc_publisher_init_best_effort(
-  // RCCHECK(rclc_publisher_init_default(
-    &publisher_internal,
-    &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-    "/teensy_to_teensy"));
+//   RCCHECK(rclc_publisher_init_best_effort(
+//   // RCCHECK(rclc_publisher_init_default(
+//     &publisher_internal,
+//     &node,
+//     ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+//     "/teensy_to_teensy"));
 
 
 
-  RCCHECK(rclc_subscription_init_best_effort(
-//  RCCHECK(rclc_subscription_init_default(
-    &subscriber_internal,
-    &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-    "/teensy_to_teensy"));
-  num_handles++;
+//   RCCHECK(rclc_subscription_init_best_effort(
+// //  RCCHECK(rclc_subscription_init_default(
+//     &subscriber_internal,
+//     &node,
+//     ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+//     "/teensy_to_teensy"));
+//   num_handles++;
 
-  RCCHECK(rclc_publisher_init_best_effort(
-//  RCCHECK(rclc_publisher_init_default(
-    &publisher,
-    &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-    "/micro_ros_arduino_node_publisher2"));
+//   RCCHECK(rclc_publisher_init_best_effort(
+// //  RCCHECK(rclc_publisher_init_default(
+//     &publisher,
+//     &node,
+//     ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+//     "/micro_ros_arduino_node_publisher2"));
+
+
+  // set up nodelet subscribers
+  int num_handles = 0;
+  for (std::tuple<rcl_timer_t, int, void *(rcl_timer_t*, int64_t)> timer_tuple : component_timers) {
+    RCCHECK(rclc_timer_init_default(
+      &timer,
+      &support,
+      RCL_MS_TO_NS(timer_timeout),
+      timer_callback));
+
+  }
+
 
 
   // create executor
@@ -126,7 +154,7 @@ void setup() {
 
 void loop() {
 //  delay(10);
-  RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10)));
+  RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(1)));
 
 //  std_msgs__msg__Int32 msg;
 //  msg.data = 1;
